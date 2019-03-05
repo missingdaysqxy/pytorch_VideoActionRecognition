475b3ebe4775f454d8a24af7e2da98ff2adb3315
liuqixuan_i <liuqixuan_i@didiglobal.com>
2019-03-05 18:01:42 +0800

    没有multiproc，有rebuild等参数的版本，可以正常运行

--- preparation/prepare_data.py
+++ preparation/prepare_data.py
@@ -13,6 +13,7 @@ import h5py
 from warnings import warn
 from pose import decode_pose, align_skeletons
 from multiprocessing import Pool
+from functools import partial
 
 PROC_EXT = ".proch5.mp4"
 assert os.path.splitext(PROC_EXT)[1] in [".mp4"], "must save as .mp4 video file!"
@@ -32,24 +33,25 @@ def _get_label(cls, file_name):
             return "unknown_positive"
 
 
-def save_skeletons2video(person_skeletons, h5_path, visualize=False):
-    proc_path = os.path.splitext(os.path.splitext(h5_path)[0])[0] + PROC_EXT
+def save_skeletons2video(person_skeletons, save_path, show_window=False):
     assert len(person_skeletons) > 0
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(proc_path, fourcc, 20, person_skeletons[0].shape[1::-1])
+    out = cv2.VideoWriter(save_path, fourcc, 20, person_skeletons[0].shape[1::-1])
     for frm in person_skeletons:
         out.write(frm)
-        if visualize:
+        if show_window:
             cv2.imshow(os.path.basename(proc_path), frm)
             if cv2.waitKey(1) == ord('q'):
                 break
                 cv2.destroyAllWindows()
     cv2.destroyAllWindows()
     out.release()
-    return proc_path
 
 
-def proc_h5(h5_path):
+def proc_h5(h5_path, zoom_factor=1.0, show_window=True, rebuild=False):
+    save_path = os.path.splitext(os.path.splitext(h5_path)[0])[0] + PROC_EXT
+    if os.path.isfile(save_path) and not rebuild:
+        return save_path
     with h5py.File(h5_path, "r") as hf:
         h, w = hf["height"].value, hf["width"].value
         assert h > 0 and w > 0
@@ -60,24 +62,24 @@ def proc_h5(h5_path):
                 continue
             joint_list = hf["frame%d" % frm]["joint_list"].value
             person_to_joint_assoc = hf["frame%d" % frm]["person_to_joint_assoc"].value
-            canvas = decode_pose(joint_list, person_to_joint_assoc, (h, w), zoom_factor=0.5)
+            canvas = decode_pose(joint_list, person_to_joint_assoc, (h, w), zoom_factor=zoom_factor)
             person_skeletons.append(canvas)
     person_skeletons = align_skeletons(person_skeletons)
-    save_path = save_skeletons2video(person_skeletons, h5_path)
+    save_skeletons2video(person_skeletons, save_path, show_window=show_window)
     return save_path
 
 
-def _walk_file(file_path):
+def _walk_file(zoom_factor, show_window, rebuild, file_path):
     name, ext = os.path.splitext(file_path)
     if ext == ".h5":
-        save_path = proc_h5(file_path)
+        save_path = proc_h5(file_path, zoom_factor, show_window, rebuild)
         print("process {} into {}".format(file_path, save_path))
         return save_path
     else:
         return None
 
 
-def prepare(root_dir, datalist_file, clslist_file):
+def prepare(root_dir, datalist_file, clslist_file, multiproc=True, zoom_factor=0.5, show_window=False, rebuild=False):
     sub_dirs = ["pos", "neg"]
     pool = Pool()
     os.makedirs(os.path.dirname(datalist_file), exist_ok=True)
@@ -88,7 +90,8 @@ def prepare(root_dir, datalist_file, clslist_file):
             for root, dirs, files in os.walk(os.path.join(root_dir, sub_dir)):
                 if len(files) == 0:
                     continue
-                join_rets = pool.map(_walk_file, [os.path.join(root, file) for file in files])
+                pa_func = partial(_walk_file,zoom_factor, show_window, rebuild)
+                join_rets = pool.map(pa_func, [os.path.join(root, file) for file in files])
                 for proc_path in join_rets:
                     if proc_path:
                         label = _get_label(sub_dir, os.path.basename(proc_path))
